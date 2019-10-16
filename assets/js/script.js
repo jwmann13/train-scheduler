4e6be6cfe0eecb0dc2d5ac2e66f2d9f309ef0685
Jeffrey Mann <jwmann22@gmail.com>
2019-10-16 18:33:26 -0400

    basically working

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBfmsPHBp2mbzTig79VtqD6R79Yn7YQS7M",
+    authDomain: "train-scheduler-96789.firebaseapp.com",
+    databaseURL: "https://train-scheduler-96789.firebaseio.com",
+    projectId: "train-scheduler-96789",
+    storageBucket: "train-scheduler-96789.appspot.com",
+    messagingSenderId: "1096571280722",
+    appId: "1:1096571280722:web:e16fd2c603fe6cfb022e8b",
+    measurementId: "G-0VE2FWVGBP"
+};
+
+$(document).ready(() => {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    let database = firebase.database();
+
+    let table = $('table');
+
+    database.ref().on('value', (snapshot) => {
+        table.empty();
+        table.html(`<tr>
+            <th>Train Name</th>
+            <th>Destination</th>
+            <th>Frequency (min)</th>
+            <th>Next Arrival</th>
+            <th>Time til Arrival</th>
+            </tr>`);
+        // let val = snapshot.val();
+
+        snapshot.forEach((childSnapshot) => {
+            let val = childSnapshot.val();
+            // console.log(childSnapshot.val());
+            table.append(`<tr>
+            <td>${val.name}</td>
+            <td>${val.destination}</td>
+            <td>${val.frequency}</td>
+            <td>${calcNextTrainTime(val.initTime, val.frequency).time}</td>
+            <td>${calcNextTrainTime(val.initTime, val.frequency).timeTil}</td>
+            <tr>`);
+        })
+    })
+
+    $('#submit').on('click', function (event) {
+        event.preventDefault();
+
+        let trainName = $('#train-name').val().trim();
+        let trainDest = $('#train-destination').val().trim();
+        let trainTime = $('#train-time').val().trim();
+        let trainFreq = $('#train-freq').val().trim();
+
+        let nextArrival = calcNextTrainTime(trainTime, trainFreq);
+
+        let newTrain = {
+            name: trainName,
+            initTime: trainTime,
+            destination: trainDest,
+            frequency: trainFreq
+        }
+
+        database.ref().push(newTrain);
+
+        console.log('name: ', trainName);
+        console.log('dest: ', trainDest);
+        console.log('time: ', trainTime);
+        console.log('next train: ', nextArrival)
+    });
+})
+
+function calcNextTrainTime(firstTime, freq) {
+    firstTime = moment(firstTime, 'HH:mm').subtract(1, "years");
+    let currentTime = moment();
+    let dTime = currentTime.diff(firstTime, 'minutes');
+    let tMod = dTime % freq;
+    let timeTilTrain = freq - tMod;
+    let nextTrain = moment().add(timeTilTrain, 'minutes').format('HH:mm');
+    return {
+        time: nextTrain,
+        timeTil: timeTilTrain
+    }
+}
