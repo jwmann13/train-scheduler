b084581052bb9b5f7bb5eb8777ae019e5a3b6efb
Jeffrey Mann <jwmann22@gmail.com>
2019-10-17 18:27:32 -0400

    had to add config.js back

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBfmsPHBp2mbzTig79VtqD6R79Yn7YQS7M",
+    authDomain: "train-scheduler-96789.firebaseapp.com",
+    databaseURL: "https://train-scheduler-96789.firebaseio.com",
+    projectId: "train-scheduler-96789",
+    storageBucket: "train-scheduler-96789.appspot.com",
+    messagingSenderId: "1096571280722",
+    appId: "1:1096571280722:web:e16fd2c603fe6cfb022e8b",
+    measurementId: "G-0VE2FWVGBP"
+};
